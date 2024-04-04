bd55d555ffc3d6ee4f446223b1956285c6e17d79
Алексей Ложкин <leha.lozhkin.02@mail.ru>
2024-04-04 19:24:28 +0300

    update genDiff.php

--- src/genDiff.php
+++ src/genDiff.php
@@ -2,12 +2,18 @@
 
 namespace Differ\Differ;
 
-function genDiff(string $pathToFile1, string $pathToFile2): string
+use Symfony\Component\Yaml\Yaml;
+
+function genDiff(string $pathToFile1, string $pathToFile2): string|null
 {
     $diff = '{';
-
-    $data1 = json_decode(file_get_contents($pathToFile1), true) ?? [];
-    $data2 = json_decode(file_get_contents($pathToFile2), true) ?? [];
+    try {
+        $data1 = getFileData($pathToFile1);
+        $data2 = getFileData($pathToFile2);
+    } catch (\Exception $e) {
+        print_r($e->getMessage());
+        return null;
+    }
 
     $added = array_diff($data2, $data1);
     $removed = array_diff($data1, $data2);
@@ -30,4 +36,17 @@ function genDiff(string $pathToFile1, string $pathToFile2): string
     }
 
     return implode("\n", [$diff, '}']);
+}
+
+function getFileData(string $filePath): array|object
+{
+    $fileExtension = pathinfo($filePath, PATHINFO_EXTENSION);
+
+    if ($fileExtension === 'json') {
+        return json_decode(file_get_contents($filePath), true) ?? [];
+    } else if ($fileExtension === 'yml' || $fileExtension === 'yaml') {
+        return Yaml::parseFile($filePath, /*Yaml::PARSE_OBJECT_FOR_MAP*/);
+    } else {
+        throw new \Exception("FormatError: unsupported file format .{$fileExtension}\n");
+    }
 }
