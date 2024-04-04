d955080a5afb40f5ac540d2ce5fac1d25b2c0396
Алексей Ложкин <leha.lozhkin.02@mail.ru>
2024-04-04 19:23:19 +0300

    update GenDiffTest.php

--- tests/GenDiffTest.php
+++ tests/GenDiffTest.php
@@ -4,9 +4,12 @@ namespace Differ\Tests;
 
 use PHPUnit\Framework\TestCase;
 use function Differ\Differ\genDiff;
+use function Differ\Differ\getFileData;
 
 class GenDiffTest extends TestCase
 {
+    public $file1 = 'filepath1.yml';
+    public $file2 = 'filepath2.yaml';
     public function testGenDiff(): void
     {
         $expected = "{
@@ -20,5 +23,23 @@ class GenDiffTest extends TestCase
 }";
 
         $this->assertEquals($expected, genDiff('file1.json', 'file2.json'));
+    }
+
+    public function testGetFileData(): void
+    {
+        $expected1 = [
+            'host' => "hexlet.io",
+            'timeout' => 50,
+            'proxy' => "123.234.53.22",
+            'follow' => false
+        ];
+        $expected2 = [
+            "timeout" => 20,
+            "verbose" => true,
+            "host" => "hexlet.io",
+            "about" => "test"
+        ];
+        $this->assertEquals($expected1, getFileData($this->file1));
+        $this->assertEquals($expected2, getFileData($this->file2));
     }
 }
