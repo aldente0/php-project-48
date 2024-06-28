3d4be035877f64c8a60b75c06675ea7ce2f3497d
Алексей Ложкин <leha.lozhkin.02@mail.ru>
2024-06-28 16:30:10 +0300

    update plain.php

--- src/Formatters/plain.php
+++ src/Formatters/plain.php
@@ -4,43 +4,39 @@ namespace Differ\Formatters;
 
 function plain($diffData, $stringFromLastLevel = '', $isFirstLevel = true): string
 {
-    $result = '';
-
-    foreach ($diffData as $data) {
+    return array_reduce($diffData, function ($res, $data) use ($stringFromLastLevel, $isFirstLevel) {
         if ($data['status'] === 'not changed') {
-            continue;
+            return $res;
         }
 
-        $row1 = $stringFromLastLevel ?: 'Property ';
+        $row = $stringFromLastLevel ?: 'Property ';
         $name = $data['name'];
 
         if (!$isFirstLevel) {
-            $row1 .= ".{$name}";
+            $row = implode('.', [$row, $name]);
         } else {
-            $row1 .= "'{$name}";
+            $row = implode('', [$row, "'{$name}"]);
         }
 
         if ($data['status'] === 'nested') {
-            $row1 = plain($data['child'], $row1, false);
+            $row = plain($data['child'], $row, false);
         } elseif ($data['status'] === 'changed') {
             $oldValue = toPlain($data['oldValue']);
             $newValue = toPlain($data['newValue']);
 
-            $row1 .= "' was updated. From {$oldValue} to $newValue\n";
+            $row = implode("'", [$row, " was updated. From {$oldValue} to $newValue\n"]);
         } else {
             $value = toPlain($data['value']);
 
             if ($data['status'] === 'deleted') {
-                $row1 .= "' was removed\n";
+                $row = implode("'", [$row, " was removed\n"]);
             } elseif ($data['status'] === 'added') {
-                $row1 .= "' was added with value: {$value}\n";
+                $row = implode("'", [$row, " was added with value: {$value}\n"]);
             }
         }
 
-        $result = implode("", [$result, $row1]);
-    }
-
-    return $result;
+        return implode("", [$res, $row]);
+    }, '');
 }
 
 function toPlain($data): string
@@ -52,7 +48,7 @@ function toPlain($data): string
     } elseif ($data === false) {
         return "false";
     } elseif (is_string($data)) {
-        return "'" . $data . "'";
+        return implode("'", ['', $data, '']);
     } elseif (is_null($data)) {
         return 'null';
     }
