82022ea09473b51272b8926501261616383047cc
Алексей Ложкин <leha.lozhkin.02@mail.ru>
2024-07-01 17:05:08 +0300

    refactor code

--- src/Formatters/stylish.php
+++ src/Formatters/stylish.php
@@ -16,7 +16,9 @@ function stylish(array $diffData, int $level = 1): string
         } elseif ($data['status'] === 'changed') {
             $newValue = toStylish($data['newValue'], $level + 1);
             $oldValue = toStylish($data['oldValue'], $level + 1);
-            $resWithValue = implode("\n", ["$resWithSpaces  - {$name}: {$oldValue}", "{$spaces}  + {$name}: {$newValue}"]);
+            $resWithValue = implode("\n",
+                ["$resWithSpaces  - {$name}: {$oldValue}", "{$spaces}  + {$name}: {$newValue}"]
+            );
         } else {
             $value = toStylish($data['value'], $level + 1);
 
